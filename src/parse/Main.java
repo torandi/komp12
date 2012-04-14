bc4c7a314d4656b57eed12386974a2156d018909
Andreas Tarandi <torandi@gmail.com>
2012-04-15 01:05:46 +0200

    Support multiple inputfiles
    complies separatly

--- src/parse/Main.java
+++ src/parse/Main.java
@@ -22,21 +22,23 @@ public class Main {
     AbstractTree abstractTree;
 
     public static void main(String[] args) {
-        Main main = new Main();
-        frameFactory = new jvm.Factory();
-        if(!main.begin(args)) {
-            System.exit(-1);
-        } 
+        for(String s : args) {
+            Main main = new Main();
+            frameFactory = new jvm.Factory();
+            if(!main.begin(s)) {
+                System.out.println("Compilation failed: "+s);
+            } 
+        }
     }
 
     public Main() {
         error = new ErrorMsg(System.err);
     }
 
-    public boolean begin(String[] args) {
+    public boolean begin(String file) {
         try {
-            Reader r = new FileReader(args[0]);
-            System.out.println("Building from "+args[0]);
+            Reader r = new FileReader(file);
+            System.out.println("Building from "+file);
             Start basicTree = parse(r);
             abstractTree = new AbstractTree(basicTree,error);
             abstractTree.build();
@@ -45,11 +47,11 @@ public class Main {
                return false;
 
             //Build abstract tree
-	    PrintWriter pw = new PrintWriter (args[0]+".syntax");
+	    PrintWriter pw = new PrintWriter (file+".syntax");
             ASTPrintVisitor pv = new ASTPrintVisitor(pw);
             pv.visit(abstractTree.program);
             pw.close();
-            System.out.println("Syntax saved to "+args[0]+".syntax");
+            System.out.println("Syntax saved to "+file+".syntax");
 
             //Check syntax and bind types
             TypeDefVisitor tdv= new TypeDefVisitor(error);
@@ -63,10 +65,10 @@ public class Main {
             if(error.anyErrors)
                return false;
             
-            pw = new PrintWriter(args[0]+".symbols");
+            pw = new PrintWriter(file+".symbols");
             ASTSymbolPrintVisitor symbolpv= new ASTSymbolPrintVisitor(new StackedTabPrinter(pw));
             symbolpv.visit(abstractTree.program);
-            System.out.println("SymbolTable saved to "+args[0]+".symbols");
+            System.out.println("SymbolTable saved to "+file+".symbols");
             
 
             pw.close();
@@ -84,7 +86,7 @@ public class Main {
 
             return true;
         } catch (FileNotFoundException ex) {
-            error.complain(args[0]+" not found.");
+            error.complain(file+" not found.");
             return false;
         } catch (ParseException ex) {
             error.complain("Parse exception in line "+ex.currentToken.beginLine+": "+ex);
