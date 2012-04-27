2d3427984c40ca8247e245b11c0b93838518e483
Andreas Tarandi <torandi@gmail.com>
2012-04-27 18:39:52 +0200

    Ehm.. some files was needed

---
+++ src/basic_tree/CustomType.java
@@ -0,0 +1,19 @@
+package basic_tree;
+
+
+public class CustomType extends Type {
+
+	private String id;
+
+	public CustomType(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	
+	
+}
