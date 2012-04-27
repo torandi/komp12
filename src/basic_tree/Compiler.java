60516145400ed027f4c48fe7ca7f0d05c626f23c
Andreas Tarandi <torandi@gmail.com>
2012-04-27 18:36:35 +0200

    Removing old crap

--- src/basic_tree/Compiler.java
+++
@@ -1,5 +0,0 @@
-package basic_tree;
-
-class Compiler {
-
-}
--- src/basic_tree/SyntaxError.java
+++
@@ -1,7 +0,0 @@
-package basic_tree;
-
-public class SyntaxError extends Exception{
-    public SyntaxError(String msg) {
-        super(msg);
-    }
-}
