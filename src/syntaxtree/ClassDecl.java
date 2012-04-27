4a615406c8ecef40dcdfaeed37737f864ebf846d
Andreas Tarandi <torandi@gmail.com>
2012-04-27 22:51:35 +0200

    Extended method lookup

--- src/syntaxtree/ClassDecl.java
+++ src/syntaxtree/ClassDecl.java
@@ -1,4 +1,5 @@
 package syntaxtree;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import symbol.Scope;
@@ -6,71 +7,71 @@ import visitor.Visitor;
 import visitor.TypeVisitor;
 
 public abstract class ClassDecl extends Scope {
-  public Identifier i;
-  public VarDeclList vl;
-  public MethodDeclList ml;
-  public frame.VMRecord record;
-  
-  public HashMap<String, ArrayList<MethodDecl>  > methods =
-                new HashMap<String,ArrayList<MethodDecl> >();
 
-  public abstract void accept(Visitor v);
-  public abstract Type accept(TypeVisitor v);
+    public Identifier i;
+    public VarDeclList vl;
+    public MethodDeclList ml;
+    public frame.VMRecord record;
+    public HashMap<String, ArrayList<MethodDecl>> methods =
+            new HashMap<String, ArrayList<MethodDecl>>();
 
-  /**
-   * Adds the specified method to the method list.
-   * @return True if successfull, False if the method was already declared
-   */
-  public boolean addMethod(Identifier id, ArrayList<Type> tl,MethodDecl m) {
-      ArrayList<MethodDecl> container = methods.get(id.s.intern());
-      if(container==null) {
-          container = new ArrayList<MethodDecl>();
-          methods.put(id.s.intern(), container);
-      }
-      if (findMethod(container, tl)==null) {
-          container.add(m);
-          m.cls = this;
-          return true;
-      } else {
-          return false;
-      }
-  }
+    public abstract void accept(Visitor v);
 
-  /**
-   * @return The method or null if it doesn't exist
-   */
-  public MethodDecl findMethod(Identifier id, ArrayList<Type> tl) {
-      ArrayList<MethodDecl> container = methods.get(id.s.intern());
-      if(container!=null) {
-        return findMethod(container, tl);
-      } else {
-          System.out.println("No method named "+id+" defined in class "+i);
-      }
-      return null;
-  }
+    public abstract Type accept(TypeVisitor v);
 
-  public String toString() {
-      return i.s;
-  }
+    /**
+     * Adds the specified method to the method list.
+     * @return True if successfull, False if the method was already declared
+     */
+    public boolean addMethod(Identifier id, ArrayList<Type> tl, MethodDecl m) {
+        ArrayList<MethodDecl> container = methods.get(id.s.intern());
+        if (container == null) {
+            container = new ArrayList<MethodDecl>();
+            methods.put(id.s.intern(), container);
+        }
+        if (findMethod(container, tl) == null) {
+            container.add(m);
+            m.cls = this;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  private MethodDecl findMethod(ArrayList<MethodDecl> list, ArrayList<Type> tl) {
+    /**
+     * @return The method or null if it doesn't exist
+     */
+    public MethodDecl findMethod(Identifier id, ArrayList<Type> tl) {
+        ArrayList<MethodDecl> container = methods.get(id.s.intern());
+        if (container != null) {
+            return findMethod(container, tl);
+        } else {
+            return null;
+        }
 
-      for(MethodDecl m : list) {
-          if(m.compareParameters(tl)) {
-              return m;
-          }
-      }
-      return null;
-  }
-  
-  /**
-   * Return name of superclass
-   */
-  public abstract String parent();
+    }
 
-  public String fullName() {
-      //Add package name here if packages are to be used
-      return i.s;
-  }
-  
+    public String toString() {
+        return i.s;
+    }
+
+    private MethodDecl findMethod(ArrayList<MethodDecl> list, ArrayList<Type> tl) {
+
+        for (MethodDecl m : list) {
+            if (m.compareParameters(tl)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return name of superclass
+     */
+    public abstract String parent();
+
+    public String fullName() {
+        //Add package name here if packages are to be used
+        return i.s;
+    }
 }
--- src/syntaxtree/ClassDeclExtends.java
+++ src/syntaxtree/ClassDeclExtends.java
@@ -1,25 +1,43 @@
 package syntaxtree;
+
+import java.util.ArrayList;
 import visitor.Visitor;
 import visitor.TypeVisitor;
 
 public class ClassDeclExtends extends ClassDecl {
-  public Identifier parent_id; //Parent
-  public ClassDecl parent;
- 
-  public ClassDeclExtends(Identifier ai, Identifier aj, 
-                  VarDeclList avl, MethodDeclList aml) {
-    i=ai; parent_id=aj; vl=avl; ml=aml;
-  }
 
-  public void accept(Visitor v) {
-    v.visit(this);
-  }
+    public Identifier parent_id; //Parent
+    public ClassDecl parent;
 
-  public Type accept(TypeVisitor v) {
-    return v.visit(this);
-  }
-  
-  public String parent() {
-      return parent.fullName();
-  }
+    public ClassDeclExtends(Identifier ai, Identifier aj,
+            VarDeclList avl, MethodDeclList aml) {
+        i = ai;
+        parent_id = aj;
+        vl = avl;
+        ml = aml;
+    }
+
+    @Override
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+
+    @Override
+    public Type accept(TypeVisitor v) {
+        return v.visit(this);
+    }
+
+    @Override
+    public String parent() {
+        return parent.fullName();
+    }
+
+    @Override
+    public MethodDecl findMethod(Identifier id, ArrayList<Type> tl) {
+        MethodDecl md = super.findMethod(id, tl);
+        if(md == null)
+            md = parent.findMethod(id, tl);
+        
+        return md;
+    }
 }
